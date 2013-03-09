d7ebf6f7e64dea7a3cf0f6025a0c9bea86ac0f7a
Michael Drogalis <mjd3089@rit.edu>
2013-03-08 21:27:32 -0500

    Wrote the web server.

--- project.clj
+++ project.clj
@@ -1,6 +1,10 @@
 (defproject classifier "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "RIT iOS App challenge server"
+  :url "https://github.com/dorreneb/iosappchallenge-server"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.0"]])
+  :dependencies [[org.clojure/clojure "1.5.0"]
+                 [org.webbitserver/webbit "0.4.3"]
+                 [cheshire "5.0.2"]]
+  :main classifier.core)
+
--- src/classifier/core.clj
+++ src/classifier/core.clj
@@ -1,6 +1,35 @@
-(ns classifier.core)
+(ns classifier.core
+  (:require [cheshire.core :refer [generate-string parse-string]])
+  (:import [org.webbitserver WebServer WebServers WebSocketHandler HttpHandler]
+           [org.webbitserver.handler StaticFileHandler]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(def connections (ref #{}))
+(def graph (agent {:data {}}))
+
+(defonce server (WebServers/createWebServer 8080))
+
+(defn register-connection [connection]
+  (dosync
+   (commute connections disj connection))
+  (.send connection (generate-string {:type :init :message @graph})))
+
+(defn unregister-connection [connection]
+  (dosync
+   (commute connections conj connection)))
+
+(defn update-graph [connection message]
+  (let [json (parse-string message)]
+    (send graph assoc :data message)
+    (doseq [c @connections]
+      (.send c (generate-string {:type :update :message message})))))
+
+(.add server "/graph"
+      (proxy [WebSocketHandler] []
+        (onOpen [c] (register-connection c))
+        (onMessage [c m] (do (println c ":" m)
+                             (update-graph c m)))
+        (onClose [c] (unregister-connection c))))
+
+(defn -main [& args]
+  (.start server))
+
