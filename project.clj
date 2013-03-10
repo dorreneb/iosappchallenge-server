2ad2d668586cd596263b43c8af814e613e468473
Michael Drogalis <mjd3089@rit.edu>
2013-03-10 19:00:46 -0400

    Clojure 1.5.1 is out :)

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/dorreneb/iosappchallenge-server"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.0"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.webbitserver/webbit "0.4.3"]
                  [cheshire "5.0.2"]
                  [dire "0.3.0"]]
