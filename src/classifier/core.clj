4a7babc9f4865e110826ef4669d4f7d15ca30c66
Michael Drogalis <mjd3089@rit.edu>
2013-03-11 17:03:07 -0400

    Pesistence works :D

--- src/classifier/core.clj
+++ src/classifier/core.clj
@@ -1,9 +1,50 @@
 (ns classifier.core
   (:require [cheshire.core :refer [generate-string parse-string]]
-            [dire.core :refer [with-pre-hook!]]
-            [classifier.persistence :refer [persist-session]])
+            [datomic.api :refer [q db] :as d]
+            [dire.core :refer [with-pre-hook!]])
   (:import [org.webbitserver WebServer WebServers WebSocketHandler HttpHandler]
            [org.webbitserver.handler StaticFileHandler]))
+
+(declare add-session-route)
+(declare add-session!)
+
+;;;;;;;;;;;; Datomic persistence ;;;;;;;;;;;;;;;;;
+
+(def uri "datomic:free://localhost:4334/graphs")
+(d/create-database uri)
+
+(defonce connection (d/connect uri))
+(def schema (read-string (slurp "database/schema.edn")))
+
+@(d/transact connection schema)
+
+(defn- update-session [session-id data]
+  (d/transact
+   connection
+   [{:graphs/session-id session-id
+     :db/id #db/id [:db.part/user]
+     :graphs/graph (pr-str data)}]))
+
+(defn all-sessions []
+  (map
+   (fn [[id]]
+     (-> connection db (d/entity id)))
+   (q '[:find ?e :where [?e :graphs/session-id]] (db connection))))
+
+(defn graph-for-session-id [id]
+  (q '[:find ?e :in $ ?id :where [?e :graphs/session-id ?id]] (db connection) id))
+
+(defn persist-session [session-id]
+  (fn [_ _ _ state]
+    (println "Updating via Datomic.")
+    (update-session session-id state)))
+
+(defn generate-routes []
+  (doseq [session (all-sessions)]
+    (add-session-route (str (:graphs/session-id session)))
+    (add-session! (str (:graphs/session-id session)) (read-string (:graphs/graph session)))))
+
+;;;;;;;;;;;;;;; In Memory Sessions ;;;;;;;;;;;;;;;;;
 
 (def sessions (ref {}))
 
@@ -35,18 +76,18 @@
   (.add server (str "/graph/" session-id)
         (proxy [WebSocketHandler] []
           (onOpen [c]      (register-connection session-id c))
-          (onMessage [c m] (update-graph (assoc (:type (parse-string m true)) :session-id session-id)))
+          (onMessage [c m] (update-graph (assoc (parse-string m true) :session-id session-id)))
           (onClose [c]     (unregister-connection session-id c)))))
 
-(defn add-session! [session-id]
+(defn add-session! [session-id initial-state]
   (dosync
-   (commute sessions assoc session-id {:connections (ref #{}) :graph (agent [])}))
+   (commute sessions assoc session-id {:connections (ref #{}) :graph (agent initial-state)}))
   (add-session-route session-id)
   (add-watch (get-in @sessions [session-id :graph]) :datomic (persist-session session-id)))
 
 (defn create-new-user-session [connection]
   (let [session-id (java.util.UUID/randomUUID)]
-    (add-session! session-id)
+    (add-session! session-id [])
     (.send connection (generate-string {:session-id session-id}))))
 
 (.add server "/create-session"
@@ -55,6 +96,8 @@
         (onMessage [c m])
         (onClose [c])))
 
+;;;;;;;;;;;;; Logging Hooks ;;;;;;;;;;;;;;;
+
 (with-pre-hook! #'register-connection
   (fn [id connection] (println "Registered connection on " id ":" connection)))
 
@@ -62,8 +105,9 @@
   (fn [id connection] (println "Unregistered connection on " id ": " connection)))
 
 (with-pre-hook! #'add-session!
-  (fn [session-id] (println "Creating new session " session-id)))
+  (fn [session-id _] (println "Creating new session " session-id)))
 
 (defn -main [& args]
+  (generate-routes)
   (.start server))
 
