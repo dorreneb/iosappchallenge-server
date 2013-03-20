e7704d2f248316a407d46ff3ca19e2da069e9b46
Michael Drogalis <mjd3089@rit.edu>
2013-03-20 16:55:38 -0400

    Info calls for Log4J on pre-hooks.

--- src/classifier/logging.clj
+++ src/classifier/logging.clj
@@ -1,105 +1,73 @@
 (ns classifier.logging
-  (:require [clojure.tools.logging :refer [info error]]
+  (:require [clojure.tools.logging :refer [info error fatal]]
             [dire.core :refer [with-pre-hook!]]
             [classifier.session :refer :all]))
 
 (with-pre-hook! #'register-connection
-  (fn [id connection]
-    (println "Registered connection on:" id)
-    (println "\t" connection)
-    (println "-------------------------------------------------")))
+  (fn [session-id connection]
+    (info "Connection " connection " registered on " session-id)))
 
 (with-pre-hook! #'unregister-connection
-  (fn [id connection]
-    (println "Unregistered connection on:" id)
-    (println "\t" connection)
-    (println "-------------------------------------------------")))
+  (fn [session-id connection]
+    (info "Connection " connection " unregistered on " session-id)))
 
 (with-pre-hook! #'add-session!
   (fn [session-id graph-name _]
-    (println "Creating new graph session:" session-id)
-    (println "\tNamed" graph-name)
-    (println "-------------------------------------------------")))
+    (info "Creating a new graph named " graph-name " on " session-id)))
 
 (with-pre-hook! #'create-box
   (fn [{:keys [session-id] :as message} connection]
-    (info "Creating box on session " session-id " with message " message)))
+    (info "Creating box on session " session-id " with " message)))
 
 (with-pre-hook! #'rename-box
   (fn [{:keys [session-id] :as message} connection]
-    (println "Received rename box event on:" session-id)
-    (println "\t" message)
-    (println "-------------------------------------------------")))
+    (info "Renaming a box on " session-id " with " message)))
 
 (with-pre-hook! #'move-box
   (fn [{:keys [session-id] :as message} connection]
-    (println "Received move box event on:" session-id)
-    (println "\t" message)
-    (println "-------------------------------------------------")))
+    (info "Moving a box on " session-id " with " message)))
 
 (with-pre-hook! #'delete-box
   (fn [graph {:keys [session-id] :as message} me]
-    (println "Received delete box event on:" session-id)
-    (println "\t" message)
-    (println "-------------------------------------------------")))
+    (info "Deleting a box on " session-id " with " message)))
 
 (with-pre-hook! #'create-connection
   (fn [graph {:keys [session-id] :as message} connection]
-    (println "Received create connection event on:" session-id)
-    (println "\t" message)
-    (println "-------------------------------------------------")))
+    (info "Creating a connection on " session-id " with " message)))
 
 (with-pre-hook! #'delete-connection
   (fn [{:keys [session-id] :as message}]
-    (println "Received delete connection event on:" session-id)
-    (println "\t" message)
-    (println "-------------------------------------------------")))
+    (info "Deleting a connection on " session-id " with " message)))
 
 (with-pre-hook! #'number-of-connections
   (fn [{:keys [session-id] :as message} connection]
-    (println "Received n-connection event on:" session-id)
-    (println "\t" message)
-    (println "-------------------------------------------------")))
+    (info "Number of connections requested by " connection " with" message)))
 
 (with-pre-hook! #'graph-revisions
   (fn [{:keys [session-id] :as message} connection]
-    (println "Received revision listing event on:" session-id)
-    (println "\t" message)
-    (println "-------------------------------------------------")))
+    (info "Listing the revisions for " session-id " with " message)))
 
 (with-pre-hook! #'spec-revision
   (fn [{:keys [session-id] :as message} connection]
-    (println "Getting revision on:" session-id)
-    (println "\t" message)
-    (println "-------------------------------------------------")))
+    (info "Requesting a revision snapshot on " session-id " with " message)))
 
 (with-pre-hook! #'revert
   (fn [{:keys [session-id transaction-id] :as message}]
-    (println "Reverting spec on:" session-id)
-    (println "\t:" message)
-    (println "-------------------------------------------------")))
-
-(with-pre-hook! #'unknown-api-call
-  (fn [message connection]
-    (println "Received an unrecognized message:")
-    (println "\t" message)
-    (println "-------------------------------------------------")))
+    (info "Reverting the spec on " session-id " with " message)))
 
 (with-pre-hook! #'list-user-session-keys
   (fn [connection]
-    (println "Listing session keys for:")
-    (println "\t" connection)
-    (println "-------------------------------------------------")))
-
-(with-pre-hook! #'unknown-session-call
-  (fn [connection message]
-    (println "Received an unrecognized session message:")
-    (println "\t" message)
-    (println "-------------------------------------------------")))
+    (info "Listing the session keys for " connection)))
 
 (with-pre-hook! #'persist-empty-graph
   (fn [session-id graph-name]
-    (println "Saving a new graph via Datomic for:")
-    (println "\t" session-id)
-    (println "-------------------------------------------------")))
+    ("Creating a new, blank spec for " session-id " named " graph-name)))
+
+(with-pre-hook! #'unknown-api-call
+  (fn [message connection]
+    (error "Received an unknown spec API call from " connection " with " message)))
+
+(with-pre-hook! #'unknown-session-call
+  (fn [connection message]
+    (error "Received an unkown session API call from " connection " with " message)))
 
