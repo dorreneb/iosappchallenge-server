c8567dda8e6d89b464cff5b98fbf936e78373779
Michael Drogalis <mjd3089@rit.edu>
2013-03-20 20:07:17 -0400

    Beginning to decomplect server code.

---
+++ src/classifier/server.clj
@@ -0,0 +1,23 @@
+(ns classifier.session
+  (:require [cheshire.core :refer [generate-string parse-string]]))
+
+(defn encode [data]
+  (generate-string data))
+
+(defn decode [data]
+  (parse-string data true))
+
+(defn respond [connection message]
+  (.send connection message))
+
+(defn broadcast
+  "Sends message out to all connections in listener."
+  [listeners message]
+  (doseq [connection listeners]
+    (respond connection message)))
+
+(defn microphone
+  "Sends message out to all listeners except speaker, on behalf of speaker."
+  [listeners speaker message]
+  (broadcast (disj listeners speaker) message))
+
